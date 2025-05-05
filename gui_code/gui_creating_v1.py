d76af0b18534595684b7a06eac5049e781253ead
Arjun Vaddadi <87330000+arjun-v1@users.noreply.github.com>
2025-05-05 12:05:27 +0530

    Update gui_creating_v1.py

--- gui_code/gui_creating_v1.py
+++ gui_code/gui_creating_v1.py
@@ -39,7 +39,7 @@ st.markdown("""
 
 co1, col2, col3 = st.columns([1, 3, 1])
 with col2:
-    st.image("gui_code/VS-logo.png", width=250)
+    st.image("gui_code/logo.png", width=250)
 
 df = pd.read_csv('gui_code/For-Sharing_Student_Assignment_Status-_-Feedback-sheet.csv')
 # Mapping of assignment-names to their respective status and comment columns
