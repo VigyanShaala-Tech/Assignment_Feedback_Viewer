91c05fdd7678b427fb0ca7e6c2734490b498f0a8
Arjun Vaddadi <87330000+arjun-v1@users.noreply.github.com>
2025-05-27 11:41:07 +0530

    Update integrated_supabase_code.py

--- gui_code/integrated_supabase_code.py
+++ gui_code/integrated_supabase_code.py
@@ -67,7 +67,7 @@ st.markdown("""
 
 co1, col2, col3 = st.columns([1, 3, 1])
 with col2:
-    st.image("gui_code/log.png", width=150)
+    st.image("gui_code/log.png", width=350)
 
 df = load_data_from_supabase()
 
