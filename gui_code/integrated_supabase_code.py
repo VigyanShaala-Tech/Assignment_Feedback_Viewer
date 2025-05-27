780f958d38ed86b2c6d080775416f45a1cb85416
Arjun Vaddadi <87330000+arjun-v1@users.noreply.github.com>
2025-05-27 12:13:06 +0530

    Update integrated_supabase_code.py

--- gui_code/integrated_supabase_code.py
+++ gui_code/integrated_supabase_code.py
@@ -173,7 +173,7 @@ else:
 
     time = datetime.now(pytz.timezone('Asia/Kolkata'))
     timestamp = time.strftime('%d-%m-%Y %H:%M:%S')
-    log_row = [timestamp, selected_college, selected_student, selected_assignment, status_text,feedback_history, current_feedback, "Viewed"]
+    log_row = [timestamp, selected_college, selected_student, selected_assignment, status_text,"Viewed"]
 
     try:
         sheet.append_row(log_row)
